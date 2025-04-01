6e9e9a2e502a0dfc22f18220f6a0b624064830e9
Keiko Iwata <oumelab@pt.rdy.jp>
2025-04-02 03:21:11 +0900

    fix: header & footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,10 @@
 export default function Footer() {
   return (
     <footer className="w-full sticky top-full bg-muted p-3 flex justify-center items-center">
-      <small className="text-muted-foreground">Created by <span className="text-green-600">@your-handle</span> &copy;2024</small>
+      <small className="text-muted-foreground">
+        Created by{" "}<a href="#" className="text-emerald-600">@your-handle</a>{" "}
+        &copy; {new Date().getFullYear()}
+      </small>
     </footer>
   );
 }
