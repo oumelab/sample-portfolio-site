07a2278939e3f0b9e079563ee6df17cd2ec8587c
Keiko Iwata <oumelab@pt.rdy.jp>
2025-04-02 03:42:22 +0900

    fix: Hero & About

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,10 +1,10 @@
 export default function About() {
   return (
-    <div id="about" className="max-w-3xl mx-auto py-28 px-4 flex justify-center gap-5">
-      <div className="w-[680px] h-auto bg-amber-200 rounded-md overflow-hidden">
+    <div id="about" className="max-w-3xl mx-auto py-28 px-4 flex flex-col-reverse sm:flex-row justify-center gap-5">
+      <div className="sm:w-[680px] h-auto bg-amber-200 rounded-md overflow-hidden">
         <img src="camp-coffee.webp" className="w-full h-full object-cover" alt="" />
       </div>
-      <div className="w-[640px] flex flex-col gap-4">
+      <div className="sm:w-[640px] flex flex-col gap-4">
         <h3 className="font-bold text-[40px]">About</h3>
         <p className="text-muted-foreground leading-7">私は、プログラミングを始めて約１年半となる、ビギナーの開発者です。<br />
         現在は、React、TypeScript、Tailwind CSS などを使用した、 フロントエンド開発のスキルアップに、情熱を注いでいます。</p>
