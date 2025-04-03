bd13a8d48c8d3bd703bec7b255694a48091a2a0f
Keiko Iwata <oumelab@pt.rdy.jp>
2025-04-04 00:57:55 +0900

    fix: Hero & About section

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -2,7 +2,8 @@ import { BASE_URL } from "@/constants"
 
 export default function About() {
   return (
-    <section id="about" className="max-w-3xl mx-auto py-28 px-4 flex flex-col-reverse sm:flex-row justify-center gap-5">
+    <section id="about" className="w-full py-28 px-4 dark:bg-zinc-800">
+      <div className="max-w-3xl mx-auto flex flex-col-reverse sm:flex-row justify-center gap-5">
       <div className="sm:w-[680px] h-auto rounded-md overflow-hidden">
         <img src={`${BASE_URL}/camp-coffee.webp`} className="w-full h-full object-cover" alt="" />
       </div>
@@ -11,6 +12,7 @@ export default function About() {
         <p className="text-muted-foreground leading-7">私は、プログラミングを始めて約１年半となる、ビギナーの開発者です。<br />
         現在は、React、TypeScript、Tailwind CSS などを使用した、 フロントエンド開発のスキルアップに、情熱を注いでいます。</p>
         <p className="text-muted-foreground">趣味は、キャンプです🏕️</p>
+      </div>
       </div>
     </section>
   )
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,19 +1,20 @@
 import {Button} from "./ui/button";
 import {FaGithub} from "react-icons/fa";
+import { SOCIAL } from "@/constants";
 
 export default function Hero() {
   return (
-    <section className="w-full mx-auto flex flex-col gap-6 justify-center items-center text-muted-foreground bg-muted py-20 px-5">
-      <p className="size-12 rounded-full grid place-content-center bg-yellow-300/30 border border-zinc-200 text-2xl">
+    <section className="w-full mx-auto flex flex-col gap-6 justify-center items-center text-muted-foreground bg-muted py-20 px-5 dark:bg-zinc-900">
+      <p className="size-12 rounded-full grid place-content-center bg-yellow-300/30 border border-zinc-200 dark:border-zinc-400 text-2xl">
         👋
       </p>
       <h2 className="text-center text-5xl sm:text-[58px] font-bold text-foreground">
         Welcome to{" "}<span className="inline-block">My Portfolio</span>
       </h2>
       <p>React と Tailwind CSS が好きな、<br className="sm:hidden" />初心者プログラマの @handle です！</p>
-      <Button className="bg-emerald-600 text-white w-10/12 sm:w-28" asChild>
-        <a href="#">
-          <FaGithub />
+      <Button className="bg-emerald-600 text-white w-10/12 sm:w-28 hover:bg-emerald-300/30" asChild>
+        <a href={SOCIAL.github} target="_blank">
+          <FaGithub className="size-6" />
           GitHub
         </a>
       </Button>
